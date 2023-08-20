37695fb84c5f36e23cc52996bd1d53e1224f1064
Nikola <105974881+nikolanan@users.noreply.github.com>
2023-08-20 20:10:11 +0300

    Add files via upload

---
+++ numbersFromNTo1/numbersFromNTo1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int number = n; number >= 1; number--) {
+
+        cout << number << endl;
+
+    }
+}
+
