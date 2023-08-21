932649c2d6db9330815bf80586801dfc947edf49
Nikola <105974881+nikolanan@users.noreply.github.com>
2023-08-21 15:44:01 +0300

    Add files via upload

---
+++ fibunacci 3/fibunacci 3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{   
+    unsigned long long previous = 0, current = 1, next;
+
+    int theWantedNumber;
+    cin >> theWantedNumber;
+
+    if (theWantedNumber == 1) {
+        cout << 1 << endl;
+    }
+
+    int i = 2;
+    while (i <= theWantedNumber) {
+
+        next = previous + current;
+        if (i == theWantedNumber) {
+            cout << next;
+        }
+        previous = current;
+        current = next;
+
+        i++;
+
+    }
+    return 0;
+}
+
