929ca9c11501a5c4f6654a7ec8669111293f5ca3
Nikola <105974881+nikolanan@users.noreply.github.com>
2023-08-27 22:06:18 +0300

    Add files via upload

---
+++ readText/ReadText.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string text;
+    cin >> text;
+
+    while (text != "Stop") {
+
+        cout << text << endl;
+        cin >> text;
+    }
+    return 0;
+}
+    
+
