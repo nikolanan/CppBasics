f81f43bfb988f6fea2c670b5d196a8a005afa46c
Nikola <105974881+nikolanan@users.noreply.github.com>
2023-08-31 23:46:27 +0300

    Add files via upload

---
+++ passwordGuess/passwordGuess.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	string password;
+	cin >> password;
+
+	if(password == "s3cr3t!P@ssw0rd"){
+		cout << "Welcome";
+	}
+	else {
+		cout << "Wrong password!";
+	}
+
+}
