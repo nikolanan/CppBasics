e3d245a6e3d0f683d6abf9fed69438f799e37bfe
Nikola <105974881+nikolanan@users.noreply.github.com>
2023-09-01 23:51:52 +0300

    Add files via upload

---
+++ sequence 2k+1/sequence 2k+1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int k = 1;
+    
+    while (k <= n) {
+
+       cout << k << endl;
+        k = (k*2) + 1;
+       
+    }
+    
+}
+
