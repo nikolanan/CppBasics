e745c8c7bfbeca2d0baf1003a767e32719675cce
Nikola <105974881+nikolanan@users.noreply.github.com>
2023-09-08 21:18:31 +0300

    Add files via upload

---
+++ topGrade/masivi 2 zad.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double arr[30];
+    int numberOfStudents;
+    cout << "Enter number of students: ";
+    cin >> numberOfStudents;
+    
+    double maxGrade = 2;
+
+    for (int i = 0; i < numberOfStudents; i++) {
+        cin >> arr[i];
+        if (arr[i] > maxGrade) {
+            maxGrade = arr[i];
+        }
+    }
+    cout << "Max grade is: " << maxGrade << endl;
+}
+
