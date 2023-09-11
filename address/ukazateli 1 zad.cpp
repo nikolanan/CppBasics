b93873fac30626c3cf6e4fddc0386e4ac30ed44c
Nikola <105974881+nikolanan@users.noreply.github.com>
2023-09-11 22:25:36 +0300

    Add files via upload

---
+++ address/ukazateli 1 zad.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    double c, d;
+    cin >> c >> d;
+
+    cout << "address a: " << &a << endl;
+    cout << "address b: " << &b << endl;
+    cout << "address c: " << &c << endl;
+    cout << "address d: " << &d << endl;
+}
+
