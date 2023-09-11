034c0efaf716c6d01e9250810f2a26c725fb9493
Nikola <105974881+nikolanan@users.noreply.github.com>
2023-09-11 22:47:57 +0300

    Add files via upload

---
+++ numbersDivisibleBy5/for 2 zad.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    n = n - 1;
+    
+    for (int i = 0; i < n; n--) {
+
+        if (n % 5 == 0) {
+            cout << n << endl;
+        }
+
+    }
+}
+
